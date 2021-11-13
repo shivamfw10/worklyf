111b9a22ccf3b0ea4f9081d936654a69b3425025
Krakash06 <akash_fw11_290@masai.school>
2021-11-13 14:32:24 +0530

    Splash Screen

---
+++ Akash_Section/Slider/dataSlider.js
@@ -0,0 +1,30 @@
+import { v4 as uuidv4 } from "uuid";
+
+const dataSlider = [
+  {
+    id: uuidv4(),
+    title: "</network>",
+    subTitle: " with like-minded people",
+    bgcolor: "rgba(58, 68, 161, 0.9)"
+  },
+  {
+    id: uuidv4(),
+    title: "</find jobs>",
+    subTitle: "WFH, hybrid and projects",
+    bgcolor: "rgba(2, 120, 88, 0.8)"
+  },
+  {
+    id: uuidv4(),
+    title: "</co-work>",
+    subTitle: " virtual and offline spaces",
+    bgcolor: "rgba(229, 38, 90, 0.8)"
+  },
+  {
+    id: uuidv4(),
+    title: "</learn>",
+    subTitle: "and grow with experts",
+    bgcolor: "rgba(255, 165, 28, 0.8)"
+  }
+];
+
+export default dataSlider;
